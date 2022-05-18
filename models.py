83f40e31cd536bc46a70877f5c9848dd7a25b8e7
Sameed <sameed.atta.khan99@gmail.com>
2022-05-18 15:04:01 +0500

    data return function has updated

--- models.py
+++ models.py
@@ -14,12 +14,14 @@ def insert_data(data=None):
     
         if data is None:
             print("data is missing to inesrt")
-            # return
-
+            return None
+        elif (data):
+            return data
+        
         #statement = text("INSERT INTO travel_db.alert_info (country_name, travel_alerts, status_date, link, information, key_encode) VALUES (:country_name, :travel_alerts, :status_date, :link, :information, :key_encode)")
-        statement = text("INSERT INTO travel_db.alert_info (country_name, travel_alerts, status_date, link, information, key_encode ) VALUES (:country_name, :travel_alerts, :status_date, :link, :information, :key_encode) ON DUPLICATE KEY UPDATE travel_alerts = travel_alerts, status_date = status_date, information = information")
+        #statement = text("INSERT INTO travel_db.alert_info (country_name, travel_alerts, status_date, link, information, key_encode ) VALUES (:country_name, :travel_alerts, :status_date, :link, :information, :key_encode) ON DUPLICATE KEY UPDATE travel_alerts = travel_alerts, status_date = status_date, information = information")
 
-        # try:
-        con.execute(statement, data)
-        print("please wait inserting data")
+    statement = text("INSERT INTO travel_db.alert_info (country_name, travel_alerts, status_date, link, information, key_encode ) VALUES (:country_name, :travel_alerts, :status_date, :link, :information, :key_encode) ON DUPLICATE KEY UPDATE travel_alerts = travel_alerts, status_date = status_date, information = information")   
+    con.execute(statement, data)
+    print("please wait inserting data")
 
