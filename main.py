d678202dea3f723b0a40fd21a6d1454f2c94b1b7
Sameed <sameed.atta.khan99@gmail.com>
2022-05-18 15:03:12 +0500

    port has been changed dynamically

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", FAST_API_DOCKER_PORT=8000, reload=True)
