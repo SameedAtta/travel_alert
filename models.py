4b931bd48fd320cdb887c046039d7c33eb50951f
Sameed <sameed.atta.khan99@gmail.com>
2022-05-25 01:02:56 +0500

    INSERT INTO querry for Canadian website has updated

--- models.py
+++ models.py
@@ -32,6 +32,6 @@ def insert_data_canadian_website(data=None):
             print("data is missing to insert")
             return
         
-        statement = text("INSERT INTO travel_db.canadian_alert_info (country_name, alert_text, last_updated, risk_heading, risk_information, country_security, criteria, health, laws, natural_disaster, canadian_key_encode ) VALUES (:country_name, :alert_text, :last_updated, :risk_heading, :risk_information, :country_security, :criteria, :health, :laws, :natural_disaster, :canadian_key_encode) ON DUPLICATE KEY UPDATE alert_text = alert_text, last_update = last_update, risk_heading = risk_heading, risk_information = risk_information, country_security = country_security, criteria = criteria, health = health")
+        statement = text("INSERT INTO travel_db.canadian_travel_info (country_name, alert_text, last_updated, risk_heading, risk_information, country_security, criteria, health, laws, natural_disaster, canadian_key_encode ) VALUES (:country_name, :alert_text, :last_updated, :risk_heading, :risk_information, :country_security, :criteria, :health, :laws, :natural_disaster, :canadian_key_encode) ON DUPLICATE KEY UPDATE alert_text = alert_text, last_updated = last_updated, risk_heading = risk_heading, risk_information = risk_information, country_security = country_security, criteria = criteria, health = health")
         con.execute(statement, data)
         print("please wait inserting data")
